f673ab58addaaa38c77ccd343e0627c943c6ac27
Braian <briantdf4@gmail.com>
2025-12-12 05:24:14 -0300

    add height and width for img

--- src/components/header/Hero.jsx
+++ src/components/header/Hero.jsx
@@ -4,7 +4,7 @@ import illustrationWorking from '../../assets/images/illustration-working.svg'
 export function Hero() {
   return (
     <div className={styles.hero}>
-      <img className={styles.heroImage} src={illustrationWorking} alt="Person working at desk with computer" />
+      <img className={styles.heroImage} loading='lazy' width="600" height="350" src={illustrationWorking} alt="Person working at desk with computer" />
       <div className={`${styles.heroInfo} container`}>
         <h1 className={styles.heroTitle}>More than just shorter links</h1>
         <p className={styles.heroText}>Build your brand's recognition and get detailed insights on how your links are performing.</p>
